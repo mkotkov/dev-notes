8d538aca5fdcac8d2ecf1362bd4304098b343c3d
mkotkov <maximkotkov@gmail.com>
2026-01-30 12:41:32 +0200

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://mkotkov.github.io/",
-  base: "/dev-notes/",
+  site: "https://mkotkov.github.io/dev-notes/",
+  base: ".",
 });
