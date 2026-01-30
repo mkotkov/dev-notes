e6cb51252a62367556b37a8f264f990272e9f834
mkotkov <maximkotkov@gmail.com>
2026-01-30 12:34:36 +0200

    Fix post links

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://mkotkov.github.io/",
-  base: "/",
+  base: "/dev-notes/",
 });
